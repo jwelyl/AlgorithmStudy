6591f6c67acd38c8a78e7616c93f55847f2cd7d7
bn-tw2020 <skaxodn97@gmail.com>
2021-05-24 00:31:37 +0900

    [ADD] 210523

---
+++ 2021.05.23/기동과보설치.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool Pillar[102][102];
+bool Bar[102][102];
+
+bool checkPillar(int x, int y) {
+    if(y == 0 || Pillar[x][y - 1]) return true;
+    if((Bar[x - 1][y] && x > 0) || Bar[x][y]) return true; 
+    return false;
+}
+
+bool checkBar(int x, int y) {
+    if(Pillar[x][y - 1] || Pillar[x + 1][y - 1]) return true;
+    if(Bar[x + 1][y] && Bar[x - 1][y] && x > 0) return true;
+    return false;
+}
+
+bool isOk(int x, int y) {
+    for(int i = max(x - 1, 0); i <= x + 1; i++) {
+        for(int j = y; j <= y + 1; j++) {
+            if(Pillar[i][j] && checkPillar(i, j) == false) return false;
+            if(Bar[i][j] && checkBar(i, j) == false) return false;
+        }
+    }
+    return true;
+}
+
+vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
+    vector<vector<int>> answer;
+    memset(Pillar, 0, sizeof(Pillar));
+    memset(Bar, 0, sizeof(Bar));
+    
+    for(auto build : build_frame) {
+        int x = build[0], y = build[1];
+        int type = build[2], cmd = build[3];
+        
+        if(type == 0) { // 기둥
+            if(cmd == 1) { // 설치
+                if(checkPillar(x, y)) Pillar[x][y] = true;
+            }
+            else { // 삭제
+                Pillar[x][y] = false;
+                if(isOk(x, y) == false) Pillar[x][y] = true;
+            }
+        }
+        else {  // 보
+            if(cmd == 1) { // 설치
+                if(checkBar(x, y)) Bar[x][y] = true;
+            }
+            else {
+                Bar[x][y] = false;
+                if(isOk(x, y) == false) Bar[x][y] = true;
+            }
+        }
+    }
+    
+    for(int x = 0; x <= n; x++) {
+        for(int y = 0; y <= n; y++) {
+            if(Pillar[x][y]) answer.push_back({x, y, 0});
+            if(Bar[x][y]) answer.push_back({x, y, 1});
+        }
+    }
+
+    return answer;
+}
