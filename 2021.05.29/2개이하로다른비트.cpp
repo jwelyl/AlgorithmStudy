a081ee65b0445bef222817bdb6fc388c86e75540
bn-tw2020 <skaxodn97@gmail.com>
2021-05-29 02:44:49 +0900

    [ADD] 210529

---
+++ 2021.05.29/2개이하로다른비트.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer;
+    
+    for(int i = 0; i < numbers.size(); i++) {
+        long long num = numbers[i];
+        if(num % 2 == 0) answer.push_back(num + 1);
+        else {
+            long long zeroBit = (num + 1) & (-num);
+            long long result = (num | zeroBit) & (~(zeroBit >> 1));
+            answer.push_back(result);
+        }
+    }
+    return answer;
+}
