c4dfcd60a5f8a268d7f64bcc089997fb93f5dc7a
Garo-o <78212696+Garo-o@users.noreply.github.com>
2021-06-01 03:55:15 +0900

    [ADD]210531

--- 210531/카드_짝_맞추기.cpp
+++ 210531/카드_짝_맞추기.cpp
@@ -1,106 +1,87 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <iostream>
 using namespace std;
-
-struct info{
-    int x, y, dir, cnt;
-    info(){info(0,0,0,0);}
-    info(int a, int b, int c, int d){
-        x=a,y=b,dir=c, cnt=d;
+typedef pair<int,int> p;
+const int INF=1000000;
+struct Info{
+    int y,x,cnt;
+    Info(int y, int x, int cnt){
+        this->y=y;
+        this->x=x;
+        this->cnt=cnt;
     }
+    Info(){Info(0,0,0);}
 };
-int dx[4]={0,0,1,-1},dy[4]={1,-1,0,0};
-int mm[4][4];
-vector<pair<int,int>> vc[7];
+int dx[4]={1,-1,0,0}, dy[4]={0,0,1,-1};
+vector<p> card[6];
+int limit;
 
+int get(int& y1,int& x1,int& y2,int& x2,const vector<vector<int>>& v, const int& check){
+    int ret=INF;
+    queue<Info> q;
+    bool visit[4][4]={false};
 
-int getDist(pair<int,int> s, pair<int,int> e, int map[][4]){
-    int sx=s.second,sy=s.first,ex=e.second,ey=e.first;
-    info visit[4][4];
-
-    for(int i=0; i<4; i++)for(int j=0; j<4; j++)visit[i][j]=info(0,0,0,10000);
+    q.push(Info(y1,x1,0));
+    visit[y1][x1]=true;
     
-    queue<info>q;
-    info start=info(sx,sy,-1,0);
-    q.push(start);
-    visit[sy][sx]=start;
     while(!q.empty()){
-        info c = q.front();
+        Info curr=q.front();
         q.pop();
-        int x=c.x,y=c.y,dir=c.dir,cnt=c.cnt;
-
+        if(curr.y==y2&&curr.x==x2)
+            ret=min(ret,curr.cnt);
         for(int i=0; i<4; i++){
-            int xx=x+dx[i],yy=y+dy[i], dd=i;
-
-            if(xx>=4||yy>=4||xx<0||yy<0)continue;
-            info n=info(xx,yy,dd,cnt+1);
-            
-            if(visit[yy][xx].cnt>n.cnt){
-                q.push(n);
-                visit[yy][xx]=n;
+            int x=dx[i]+curr.x, y=dy[i]+curr.y;
+            if(x<0||y<0||x>3||y>3)continue;
+            Info next = Info(y,x,curr.cnt+1);
+            if(!visit[y][x]){
+                q.push(next);
+                visit[y][x]=true;
             }
-        }
+            while(x<=3&&y<=3&&x>=0&&y>=0){
+                if(v[y][x]>0&&!(check&1<<(v[y][x]-1)))break;
+                x+=dx[i],y+=dy[i];
+                if(x<0||y<0||x>3||y>3){
+                    x-=dx[i],y-=dy[i];break;
+                }
+            }
+            next.y=y;next.x=x;
+            if(!visit[y][x]){
+                q.push(next);
+                visit[y][x]=true;
+            }
+        }   
+    }
+    return ret;
+}
+
+int recur(int r, int c,int visit, const vector<vector<int>> v){
+    if(visit==limit){
+        return 0;
+    }
+    int ret=INF*10;
+    for(int i=0; i<6; i++)if(limit&(1<<i))if(!(visit&(1<<i))){
+        int y1=card[i][0].first,y2=card[i][1].first
+            ,x1=card[i][0].second,x2=card[i][1].second;
         
-        for(int i=0; i<4; i++){
-            int xx= x+dx[i], yy=y+dy[i];
-            if(xx<0||yy<0||xx>3||yy>3)continue;
-            while(map[yy][xx]==0||xx<3||yy<3||xx>0||yy>0){
-                xx+=dx[i];yy+=dy[i];
-            }
-            info n=info(xx,yy,i,cnt+1);
-            if(visit[yy][xx].cnt>n.cnt){
-                q.push(n);
-                visit[yy][xx]=n;
-            }
-        }
+        int tt1=get(r,c,y1,x1,v,visit)+get(y1,x1,y2,x2,v,visit);
+        int tt2=get(r,c,y2,x2,v,visit)+get(y2,x2,y1,x1,v,visit);
+        tt1+=recur(y2,x2,visit|(1<<i),v)+2;
+        tt2+=recur(y1,x1,visit|(1<<i),v)+2;
+        int temp=min(tt1,tt2);
+        ret=min(temp,ret);
     }
-    return visit[ey][ex].cnt;
-}
-int card(int c,pair<int,int> curr){
-    if(c==0)return 0;
-    int res=10000000;
-    int tmap[4][4]={0,};
-
-    for(int i=0; i<4; i++)for(int j=0; j<4; j++){
-        if(c&(1<<mm[i][j]))tmap[i][j]=mm[i][j];
-        else tmap[i][j]=0;
-    }
-    
-    for(int i=1; i<=6; i++){
-        if((1<<i)&c){
-            int temp,temp2,tt;
-
-            temp=getDist(curr,vc[i][0],tmap)+getDist(vc[i][0],vc[i][1],tmap);
-            temp2=getDist(curr,vc[i][1],tmap)+getDist(vc[i][1],vc[i][0],tmap);
-            
-            cout<<i<<" "<<curr.first<<" "<<curr.second<<" "<<min(temp,temp2)<<'\n';
-            
-            int tres=temp+card(c&(~(1<<i)),vc[i][1]);
-            int tres2=temp2+card(c&(~(1<<i)),vc[i][0]);
-            tres=min(tres,tres2);
-            
-            res=min(res,tres);
-        }
-    }
-    return res;
+    return ret;
 }
 
-int solution(vector<vector<int>> board, int r, int cc) {
-    pair<int,int> start={r,cc};
-    int answer = 0,c=0,cnt=0;
-    for(int i=0; i<4; i++)for(int j=0; j<4; j++){
-        mm[i][j]=board[i][j];
-        if(mm[i][j]>0){
-            cnt++;
-            if(!((1<<mm[i][j])&c))
-                c|=(1<<mm[i][j]);
-            vc[mm[i][j]].push_back({i,j});
+int solution(vector<vector<int>> board, int r, int c) {
+    int answer = 0;
+    for(int i=0; i<board.size();i++)for(int j=0; j<board[i].size(); j++){
+        if(board[i][j]>0){
+            card[board[i][j]-1].emplace_back(i,j);
+            limit|=(1<<(board[i][j]-1));
         }
     }
-    
-    answer=card(c,start);
-
-    return answer+cnt;
+    answer=recur(r,c,0,board);
+    return answer;
 }
