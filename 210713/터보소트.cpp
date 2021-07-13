e085fbf5cd81fe677c1eb69fd0b75441271dfd6f
Garo-o <78212696+Garo-o@users.noreply.github.com>
2021-07-13 23:29:50 +0900

    [ADD]210713

--- 210713/터보소트.cpp
+++ 210713/터보소트.cpp
@@ -2,39 +2,38 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-const int MAX=1<<18;
+const int MAX=1<<17;
 typedef pair<int,int> p;
-vector<p> arr;
-int seg[MAX], S=MAX/2;
-void update(int n){
-  while(n>1){
-    n/=2;
-    seg[n]=seg[n*2]+seg[n*2+1];
+int arr[MAX], idx[MAX];
+
+void update(int n, int v){
+  while(n<MAX){
+    arr[n]+=v;
+    n+=n&-n;
   }
 }
-int sum(int s, int e, int n, int ss, int ee){
-  if(e<ss||ee<s)return 0;
-  if(s<=ss&&ee<=e)return seg[n];
-  int mid=(ss+ee)/2;
-  return sum(s,e,n*2,ss,mid)+sum(s,e,n*2+1,mid+1,ee);
+int sum(int n){
+  int res=0;
+  while(n){
+    res+=arr[n];
+    n-=n&-n;
+  }
+  return res;
 }
+
 int main(void){
   cin.tie(0);
   ios_base::sync_with_stdio(0);
   int N; cin>>N;
-  for(int i=0; i<N; i++){
-    seg[S+i]=1;
-    int n; cin>>n; arr.push_back({n,i});
+  for(int i=1; i<=N; i++){
+    int n; cin>>n;
+    idx[n]=i; update(i,1);
   }
-  for(int i=S-1; i>0; i--)seg[i]=seg[i*2]+seg[i*2+1];
-  sort(arr.begin(),arr.end());
-  int f=0, l=N-1;
+  int f=1, l=N;
   while(f<l){
-    int idx=arr[f++].second, id=arr[l--].second;
-    seg[S+idx]=0; update(S+idx);
-    cout<<sum(0,idx,1,0,S-1)<<'\n';
-    seg[S+id]=0; update(S+id);
-    cout<<sum(id,N-1,1,0,S-1)<<'\n';
+    int id=idx[f++], id2=idx[l--];
+    update(id,-1);cout<<sum(id)<<'\n';
+    update(id2,-1);cout<<sum(N)-sum(id2)<<'\n';
   }
   if(f==l)cout<<0;
 }
